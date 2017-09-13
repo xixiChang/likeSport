f3825a3489b177105e9296fb80fdf4aed8e6d457
chenglong.cai <chenglong.cai@tcl.com>
2017-09-13 09:33:53 +0800

    test1

--- app/src/main/java/ccc/tcl/com/sprotappui/activity/layouttActivity.java
+++ app/src/main/java/ccc/tcl/com/sprotappui/activity/layouttActivity.java
@@ -2,14 +2,17 @@ package ccc.tcl.com.sprotappui.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import ccc.tcl.com.sprotappui.R;
 
-public class layouttActivity extends AppCompatActivity {
+public class layouttActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_layoutt);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        super.setToolBar(toolbar, R.string.activity_main_title);
     }
 }
