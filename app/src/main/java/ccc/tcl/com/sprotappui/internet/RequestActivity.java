34b725f192348f6c02f7a2c4f07bf1deff1f18ce
xi-zhang <xi-zhang@tcl.com>
2017-09-28 15:27:30 +0800

    完善Activity所有请求接口（８个）

--- app/src/main/java/ccc/tcl/com/sprotappui/internet/RequestActivity.java
+++ app/src/main/java/ccc/tcl/com/sprotappui/internet/RequestActivity.java
@@ -1,6 +1,7 @@
 package ccc.tcl.com.sprotappui.internet;
 
 import java.util.List;
+import java.util.Map;
 
 import ccc.tcl.com.sprotappui.model.PlatFormActivity;
 import ccc.tcl.com.sprotappui.model.ResponseResult;
@@ -8,6 +9,7 @@ import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
 import retrofit2.http.Body;
 import retrofit2.http.Field;
+import retrofit2.http.FieldMap;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
@@ -15,7 +17,12 @@ import retrofit2.http.Part;
 import rx.Observable;
 
 import static ccc.tcl.com.sprotappui.constant.URLConstant.Activity_All;
+import static ccc.tcl.com.sprotappui.constant.URLConstant.Activity_Cancel;
+import static ccc.tcl.com.sprotappui.constant.URLConstant.Activity_Delay;
 import static ccc.tcl.com.sprotappui.constant.URLConstant.Activity_Details;
+import static ccc.tcl.com.sprotappui.constant.URLConstant.Activity_GetJoinerInfo;
+import static ccc.tcl.com.sprotappui.constant.URLConstant.Activity_Join;
+import static ccc.tcl.com.sprotappui.constant.URLConstant.Activity_My;
 import static ccc.tcl.com.sprotappui.constant.URLConstant.Activity_Post;
 import static ccc.tcl.com.sprotappui.constant.URLConstant.File_Upload;
 
@@ -29,10 +36,33 @@ public interface RequestActivity {
     @POST(value = Activity_Post)
     Observable<ResponseResult> uploadActivity(@Body PlatFormActivity activity);
 
+    @POST(value = Activity_Delay)
+    @FormUrlEncoded
+    Observable<ResponseResult> delayActivity(@FieldMap Map<String, String> delayInfo);
+
+
+    @POST(value = Activity_Cancel)
+    @FormUrlEncoded
+    Observable<ResponseResult> cancelActivity(@Field("at_server_id") String at_server_id,
+                                              @Field("reason") String reason);
+
     @POST(value = Activity_Details)
     @FormUrlEncoded
-    Observable<ResponseResult> getActivity(@Field("at_server_id") String at_server_id);
+    Observable<ResponseResult<PlatFormActivity>> getActivity(@Field("at_server_id") String at_server_id);
+
+    @POST(value = Activity_My)
+    Observable<ResponseResult<List<PlatFormActivity>>> myActivity();
+
+
+    @POST(value = Activity_Join)
+    @FormUrlEncoded
+    Observable<ResponseResult> joinActivity(@Field("at_server_id") String at_server_id);
+
 
     @POST(value = Activity_All)
     Observable<ResponseResult<List<PlatFormActivity>>> getAll();
+
+    @POST(value = Activity_GetJoinerInfo)
+    @FormUrlEncoded
+    Observable<ResponseResult> getJoinerInfo(@Field("userIdList") String users);
 }
--- app/src/main/java/ccc/tcl/com/sprotappui/presenter/presenterimpl/ActivityPresenter.java
+++ app/src/main/java/ccc/tcl/com/sprotappui/presenter/presenterimpl/ActivityPresenter.java
@@ -1,6 +1,9 @@
 package ccc.tcl.com.sprotappui.presenter.presenterimpl;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 import ccc.tcl.com.sprotappui.internet.RequestActivity;
 import ccc.tcl.com.sprotappui.internet.RequestRecord;
 import ccc.tcl.com.sprotappui.internet.requestImpl.RetrofitHelper;
@@ -31,18 +34,71 @@ public class ActivityPresenter extends BasePresenter {
         requestActivity = helper.retrofit.create(RequestActivity.class);
     }
 
+
+
+    public static final String Activity_Join = "/activity/join";
+    public static final String Activity_All = "/activity/all";
+    public static final String Activity_GetJoinerInfo = "/activity/getJoinerInfo";
+
+
+
     public void uploadActivity(PlatFormActivity activity){
         resultObservable = requestActivity.uploadActivity(activity);
         mCompositeSubscription.add(subscribeData());
     }
 
-    public void getActivity(String activity_id){
-        resultObservable = requestActivity.getActivity(activity_id);
+
+    public void delayActivity(String at_server_id,
+                              String reason,
+                              String start_time,
+                              String end_time){
+        Map<String, String> map = new HashMap();
+        map.put("at_server_id", at_server_id);
+        map.put("reason", reason);
+        map.put("start_time", start_time);
+        map.put("end_time", end_time);
+        resultObservable = requestActivity.delayActivity(map);
         mCompositeSubscription.add(subscribeData());
     }
 
+    public void cancelActivity(String at_server_id,
+                               String reason){
+        resultObservable = requestActivity.cancelActivity(at_server_id, reason);
+        mCompositeSubscription.add(subscribeData());
+    }
+
+
+    public void getActivity(String activity_id){
+        resultObservable = (Observable) requestActivity.getActivity(activity_id);
+        mCompositeSubscription.add(subscribeData());
+    }
+
+
+    public void getMyActivity(){
+        resultObservable = (Observable) requestActivity.myActivity();
+        mCompositeSubscription.add(subscribeData());
+    }
+
+
+    public void joinActivity(String at_server_id){
+        resultObservable = (Observable) requestActivity.joinActivity(at_server_id);
+        mCompositeSubscription.add(subscribeData());
+    }
+
+
+
     public void getAll(){
         resultObservable = (Observable) requestActivity.getAll();
+        mCompositeSubscription.add(subscribeData());
+    }
+
+
+    /**
+     * users = platformActivity.getJoiner
+     * @param users
+     */
+    public void getJoinerInfo(String users){
+        resultObservable = (Observable) requestActivity.getJoinerInfo(users);
         mCompositeSubscription.add(subscribeData());
     }
 
